88e742fc388919d2a629ce59dbad4b7f49095a07
otherotherjosh <itsotherjosh@gmail.com>
2025-06-24 12:05:54 +1200

    add smooth player movement

--- player/player.gd
+++ player/player.gd
@@ -1,25 +1,23 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var speed := 300.0
+@export var acceleration := 1.0
+@export var deceleration := 1.0
+
+var curr_speed := 0.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity = lerp(velocity, direction * speed, delta * acceleration)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity = lerp(velocity, Vector2.ZERO, delta * deceleration)
+		
+	if velocity.length() > speed:
+		velocity = velocity.normalized() * speed
 
 	move_and_slide()
